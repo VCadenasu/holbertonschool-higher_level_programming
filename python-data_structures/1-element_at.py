0cbc51e04daf1c96c2e1554259567e67de2e56ec
VCadenasu <vlcadenasu@hotmail.com>
2023-09-20 20:49:10 -0700

    file 1

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 
 def element_at(my_list, idx):
-    n = len(my_list) - 1
+    n = len(my_list)
     if idx < 0:
         return None
-    elif idx > n:
+    elif idx > n - 1:
         return None
     else:
         return(my_list[idx])
