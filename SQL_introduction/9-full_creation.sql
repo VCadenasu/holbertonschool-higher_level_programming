12446d439627bdbcdf85807ad59ef0b55a72eb7f
VCadenasu <vlcadenasu@hotmail.com>
2023-12-10 18:51:27 -0800

    add file 9

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,6 +1,6 @@
 -- Create a second table with multiples rows.
-CREATE TABLE IF NOT EXISTS second_table (id INT PRIMARY KEY AUTO_INCREMENT, name VARCHAR(256), score INT);
-INSERT INTO second_table (name, score) VALUES ('John', 10);
-INSERT INTO second_table (name, score) VALUES ('Alex', 3);
-INSERT INTO second_table (name, score) VALUES ('Bob', 14);
-INSERT INTO second_table (name, score) VALUES ('George', 8);
+CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
+INSERT INTO second_table (id, name, score) VALUES (1, 'John', 10);
+INSERT INTO second_table (id, name, score) VALUES (2, 'Alex', 3);
+INSERT INTO second_table (id, name, score) VALUES (3, 'Bob', 14);
+INSERT INTO second_table (id, name, score) VALUES (4, 'George', 8);
